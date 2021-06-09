b890be02a6bf4346a0899b59a11db9896d6c8193
Campbell Vertesi <campbell@vertesi.com>
2021-06-09 14:12:16 +0000

    Add test log buffer tool

---
+++ tools/tools.go
@@ -0,0 +1,48 @@
+package tools
+
+import (
+	"bytes"
+	"log"
+	"strings"
+	"testing"
+)
+
+// An in-memory log store for testing log outputs.
+
+type TestLogBuffer struct {
+	GotBuffer    *bytes.Buffer
+	ExpectBuffer *bytes.Buffer
+}
+
+// Add a log string to the list of expected log outputs.
+func (b TestLogBuffer) ExpectLog(s string) error {
+	_, err := b.ExpectBuffer.WriteString(s)
+	if err != nil {
+		return err
+	}
+	_, err = b.ExpectBuffer.WriteString("\n")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Tests the received log against the expectations.
+func (b *TestLogBuffer) TestLogValues(t *testing.T) {
+	if strings.Compare(b.GotBuffer.String(), b.ExpectBuffer.String()) != 0 {
+		gotLog := b.GotBuffer.String()
+		wantLog := b.ExpectBuffer.String()
+		t.Errorf("Got wrong log output. Got %+q\n want %+q\n", gotLog, wantLog)
+	}
+}
+
+// CreateAndActivateEmptyTestLogBuffer creates a new TestLogBuffer and applies it to the standard log library.
+func CreateAndActivateEmptyTestLogBuffer() *TestLogBuffer {
+	logBuffer := TestLogBuffer{
+		&bytes.Buffer{},
+		&bytes.Buffer{},
+	}
+	log.SetOutput(logBuffer.GotBuffer)
+	log.SetFlags(0)
+	return &logBuffer
+}
